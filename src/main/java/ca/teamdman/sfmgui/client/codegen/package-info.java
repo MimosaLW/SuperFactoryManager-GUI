eb99e731fb936df026339c9a7bf122dbf0f21a39
小飘 <110318627+mynamexiaopiao@users.noreply.github.com>
2026-08-02 00:32:45 +0800

    整理26.1.2分支目录结构

--- src/main/java/ca/teamdman/sfmgui/client/codegen/package-info.java
+++
@@ -1,9 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-@FieldsAreNonnullByDefault
-package ca.teamdman.sfmgui.client.codegen;
-
-import net.minecraft.FieldsAreNonnullByDefault;
-import net.minecraft.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
--- src/main/java/ca/teamdman/sfmgui/client/model/package-info.java
+++
@@ -1,9 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-@FieldsAreNonnullByDefault
-package ca.teamdman.sfmgui.client.model;
-
-import net.minecraft.FieldsAreNonnullByDefault;
-import net.minecraft.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
